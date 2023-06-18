0dceabd8e87b55e898ed19a83743744ed5009642
michalmazurdev <m.mazur40@gmail.com>
2023-06-18 23:37:48 +0200

    almost done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,25 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
+    filter: '',
   };
 
   addContact = async event => {
     event.preventDefault();
     const { name, number } = event.currentTarget;
+    console.log('name', name);
+    const { contacts } = this.state;
+    const contactNames = contacts.map(contact => contact.name);
+    if (contactNames.includes(name.value)) {
+      return alert(`${name.value} is alredy in contacts`);
+    }
     await this.setState(prevState => ({
       contacts: [
         ...prevState.contacts,
@@ -18,8 +30,17 @@ class App extends Component {
     }));
     event.target.reset();
   };
+  handleFilterChnage = event => {
+    this.setState({ filter: event.target.value });
+  };
+  filterArrayByName = event => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
   render() {
-    console.log(this.state);
+    // console.log(this.state);
 
     return (
       <div
@@ -59,18 +80,29 @@ class App extends Component {
           <button type="submit">Add contact</button>
         </form>
         {/* Phonebook  */}
-        {/* contacts  */}
+        {/* filter */}
+        <label>
+          Find contacts by name
+          <input
+            onChange={this.handleFilterChnage}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Z]+(([' \u2013][a-zA-Z])?[a-zA-Z]*)*$"
+          />
+        </label>
+        {/* filter */}
+        {/* ContactList  */}
         <div>
           <h2>Contacts</h2>
           <ul>
-            {this.state.contacts.map(contact => (
+            {this.filterArrayByName().map(contact => (
               <li key={nanoid()}>
-                {contact.name} {contact.number}{' '}
+                {contact.name} {contact.number}
               </li>
             ))}
           </ul>
         </div>
-        {/* contacts  */}
+        {/* ContactList  */}
       </div>
     );
   }
