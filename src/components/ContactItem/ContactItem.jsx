01469036027c109b46baf97681625803bef8c178
michalmazurdev <m.mazur40@gmail.com>
2023-06-19 03:58:56 +0200

    stylying

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,7 +3,9 @@ import css from './ContactItem.module.css';
 export const ContactItem = ({ arrayOfContacts, deleteFunction }) =>
   arrayOfContacts.map(contact => (
     <li className={css.listItem} key={contact.id}>
-      {contact.name}: {contact.number}{' '}
+      <span>
+        {contact.name}: {contact.number}{' '}
+      </span>
       <button className={css.button} onClick={() => deleteFunction(contact.id)}>
         Delete
       </button>
